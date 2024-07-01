bb67a2cf9f628b4cca865551ef85c5417f3d72a1
Piyush Shukla <panditofcodes@gmail.com>
2024-07-02 00:20:11 +0530

    index.js setup done

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+PORT = 8080
+
+app.get('/',(req,res) => {
+    res.send('Hello from home')
+} )
+
+app.listen(PORT,()=>{
+    console.log('Working!')
+})
