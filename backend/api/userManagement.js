e0bdfa28964dcd82d4ab74ff3ecd29c98fb5b747
Piyush Shukla <74832139+panditofcodes@users.noreply.github.com>
2024-07-04 17:43:54 +0530

    userRegistration api added

---
+++ backend/api/userManagement.js
@@ -0,0 +1,32 @@
+const User = require("../models/userSchema");
+
+const userRegistration = async (res, req) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ error: "Please provide email and password." });
+  }
+
+  try {
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ error: "You already have an account." });
+    }
+
+    const newUser = new User({ email, password });
+    const registeredUser = await newUser.save();
+
+    console.log(registeredUser);
+
+    return res.status(201).json({ registeredUser });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const updateUserDetails = async (res, req) => {};
+
+const deleteUser = async (res, req) => {};
